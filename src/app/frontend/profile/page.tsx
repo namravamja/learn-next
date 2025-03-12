5e2d7ec56df503c0a84ce303065f95f2e562a4b2
NamraVamja <namravamja00@gmail.com>
2025-03-12 17:26:48 +0530

    Implement logout functionality with API endpoint and update profile page

--- src/app/frontend/profile/page.tsx
+++ src/app/frontend/profile/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-function page() {
-  return <div>after login
-    
-  </div>;
+function Page() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/backend/api/logout");
+      router.push("/frontend/login");
+    } catch (e: any) {
+      console.log(e.message);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <button
+        className="px-4 py-2 text-white bg-amber-900 rounded-md hover:bg-amber-700"
+        onClick={logout}
+      >
+        LogOut
+      </button>
+    </div>
+  );
 }
 
-export default page;
+export default Page;
